05ae09fb8494cf58178833be637edbdb134651a6
Raj Ashish Desai <rajdesai94@gmail.com>
2019-01-30 20:45:59 -0500

    add heroku db connection

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
 const CREDENTIALS = require('../creds');
 
-module.exports = new Sequelize('srujana', CREDENTIALS.DB_USER, CREDENTIALS.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
+const POSTGRES = 'postgres';
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+const sequelize = process.env.DATABASE_URL ?
+  new Sequelize(process.env.DATABASE_URL, {
+    dialect: POSTGRES,
+    protocol: POSTGRES
+  })
+  : new Sequelize('srujana', CREDENTIALS.DB_USER, CREDENTIALS.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: POSTGRES,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    operatorsAliases: false
+  });
 
-  operatorsAliases: false
-});
+module.exports = sequelize;
