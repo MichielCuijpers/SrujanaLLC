baa4e1fb321bf61ec37a97a3cf2c57ded498e61f
Raj Ashish Desai <rajdesai94@gmail.com>
2019-02-04 21:21:51 -0500

    db refactor

--- config/db.js
+++ config/db.js
@@ -2,21 +2,14 @@ const Sequelize = require('sequelize');
 
 const POSTGRES = 'postgres';
 
-const sequelize = process.env.DATABASE_URL ?
-  new Sequelize(process.env.DATABASE_URL, {
-    dialect: POSTGRES,
-    protocol: POSTGRES
-  })
-  : new Sequelize('srujana', process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: POSTGRES,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    operatorsAliases: false
-  });
-
-module.exports = sequelize;
+module.exports = new Sequelize(process.env.DATABASE_URL,{
+  dialect: POSTGRES,
+  protocol: POSTGRES,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
+});
